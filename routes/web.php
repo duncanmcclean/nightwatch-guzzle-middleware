466df5838791938403c043e80bbfe752dff0f53f
Duncan McClean <duncan@duncanmcclean.com>
2025-07-03 12:23:04 +0100

    simple guzzle try/catch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $client = new \GuzzleHttp\Client();
+
+    try {
+        $client->request('GET', 'https://laravel.com/404');
+
+        dd('Should only get here if the request was successful.');
+    } catch (ClientException) {
+        dd('Request failed, as expected.');
+    }
 });
