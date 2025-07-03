96a661bed1cd84b1f8294bf76e6eb63ea18b982b
Duncan McClean <duncan@duncanmcclean.com>
2025-07-03 12:25:18 +0100

    add nightwatch guzzle middleware. the try/catch handling is broken

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 use Illuminate\Support\Facades\Route;
+use Laravel\Nightwatch\Facades\Nightwatch;
 
 Route::get('/', function () {
-    $client = new \GuzzleHttp\Client();
+    $stack = new HandlerStack;
+    $stack->setHandler(new CurlHandler);
+
+    $stack->push(Nightwatch::guzzleMiddleware());
+
+    $client = new Client(['handler' => $stack]);
 
     try {
         $client->request('GET', 'https://laravel.com/404');
 
-        dd('Should only get here if the request was successful.');
+        dd(
+            'Should only get here if the request was successful.',
+            'But... it was not and we got here anyway. Something iffy is going on.',
+            'Everything works if you comment out the dd.',
+        );
     } catch (ClientException) {
         dd('Request failed, as expected.');
     }
